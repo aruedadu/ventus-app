7a45bdf369cc1ee88ebcecede483ae6cfe5aac65
Laura Salazar <laurasalazarh@gmail.com>
2020-06-23 21:46:38 -0500

    Updates

---
+++ precache-manifest.2528812483947c4f882062116ea79759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2336ccb9ac6d52d76bba05fbf662db",
+    "url": "/ventus-app/index.html"
+  },
+  {
+    "revision": "cae4a87aa37929525198",
+    "url": "/ventus-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "79fd3483d918f2817964",
+    "url": "/ventus-app/static/js/2.cfb2c315.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ventus-app/static/js/2.cfb2c315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae4a87aa37929525198",
+    "url": "/ventus-app/static/js/main.0669afbe.chunk.js"
+  },
+  {
+    "revision": "237b090d80cdb9d363c3",
+    "url": "/ventus-app/static/js/runtime-main.0c94282f.js"
+  }
+]);
